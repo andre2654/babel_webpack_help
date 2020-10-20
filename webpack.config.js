01b1bd348dc4b412d41fa4524f61d14d9f3bc657
Andre S <andre.victor29@hotmail.com>
2020-10-20 12:20:47 -0300

    Webpack better comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
+/**
+ * ! Para iniciar execute: webpack --mode development
+ * ! Para iniciar o servidor execute: webpack-dev-server --mode development
+ */
+
 const path = require("path");
 
 module.exports = {
-	entry: path.resolve(__dirname, "src", "index.js"),
+	entry: path.resolve(__dirname, "src", "index.js"), // ! Entrada
 	output: {
-		path: path.resolve(__dirname, "public"),
-		filename: "bundle.js",
+		path: path.resolve(__dirname, "public"), // ! Saida
+		filename: "bundle.js", // ! Nome do arquivo da saida
 	},
+	// ! Inicia o servidor para reload
 	devServer: {
 		contentBase: path.resolve(__dirname, "public"),
 	},
+	// ! Regras
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				// ! entende como converte javascript
+				test: /\.js$/, // ? procura por uma arquivos que terminam com .js
 				exclude: /node_modules/,
 				use: {
-					loader: "babel-loader",
+					loader: "babel-loader", // Converte importações do js para 1 arquivo
 				},
+			},
+			{
+				test: /\.css$/, // ? procura por uma arquivos que terminam com .css
+				exclude: /node_modules/,
+				use: [
+					{ loader: "style-loader" }, // Pega o resultado do css-loader e injeta no html
+					{ loader: "css-loader" }, // Converta importações dentro do css
+				],
 			},
 		],
 	},
